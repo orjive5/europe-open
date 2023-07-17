c9fa0b116321d3912f17613af70964d3d9a9f1e8
German Dojcinovic <germandoicinovic@gmail.com>
2023-07-17 16:24:45 +0200

    finished

---
+++ sanity.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "sanity";
+import {deskTool} from 'sanity/desk';
+import schemas from './sanity/schemas';
+
+const config = defineConfig ({
+    projectId: '7fvn7aji',
+    dataset: 'production',
+    title: 'My Personal Website',
+    apiVersion: '2023-07-16',
+    basePath: '/admin',
+    plugins: [deskTool()],
+    schema: {
+        types: schemas
+    }
+});
+
+export default config;
---
+++ sanity/config/client-config.ts
@@ -0,0 +1,6 @@
+const config = {
+    projectId: '7fvn7aji',
+    dataset: 'production',
+    apiVersion: '2023-07-16',
+}
+export default config;
