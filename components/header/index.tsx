4faec654e45fd8b30b01f51975b6e5466ef7156c
German Dojcinovic <germandoicinovic@gmail.com>
2023-09-10 13:23:21 +0200

    added header navigation menu

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,26 +1,56 @@
+'use client'
+
 import { Page } from "@/types/page";
 import Image from "next/image";
 import Link from "next/link";
 import { ModeToggle } from "../modeToggle";
+import { Separator } from "@/components/ui/separator";
+import { NavMenu } from "../navMenu";
 import { Button } from "../ui/button";
 
 const Header = ({pages}: {pages: Page[]}) => {
 
   return (
-    <header className="flex justify-between bg-secondaryBg px-9">
+    <header className="flex justify-between items-center px-9">
       <Link href="/" className="flex items-center gap-5 py-7 px-9">
-        <Image
-            src="/qualion-logo.png"
-            alt="Logo"
-            width="64"
-            height="64"
-        />
+        <div className="relative w-16 h-16">
+          <Image
+              src="/europe-logo.png"
+              alt="Europe Open logo"
+              priority={true}
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              className="object-cover box-border overflow-hidden"
+          />
+        </div>
         <div className="flex flex-col h-full justify-center">
-            <h1 className="responsive-heading font-extrabold font-nunito">Europe Open</h1>
-            <p className="responsive-base text-secondaryTxt">online music competition</p>
+            <h1 className="font-semibold text-xl">
+              Europe Open
+            </h1>
+            <Separator />
+            <p className="">
+              online music competition
+            </p>
         </div>
       </Link>
-      <div className="flex items-center">
+      <NavMenu />
+      <div>
+        <Button variant='outline'>Apply</Button>
+        <ModeToggle />
+      </div>
+      {/* <div className="flex items-center">
+        {examplePages.map((page) => (
+          <Link
+            key={page}
+            href={`/${page}`}
+            className="py-7 px-9 responsive-base hover:bg-hoverBg h-full flex items-center"
+          >
+            {page}
+          </Link>
+        ))}
+      </div> */}
+      {/* pages from sanity */}
+      {/* <div className="flex items-center">
         {pages.map((page) => (
           <Link
             key={page._id}
@@ -30,9 +60,7 @@ const Header = ({pages}: {pages: Page[]}) => {
             {page.title}
           </Link>
         ))}
-      </div>
-      <Button>This is Shadcn button</Button>
-      <ModeToggle />
+      </div> */}
     </header>
   );
 };
