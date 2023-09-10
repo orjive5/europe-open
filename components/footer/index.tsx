c8814018b82e99e9c075f5e9f59b0c1fa02ec52a
German Dojcinovic <germandoicinovic@gmail.com>
2023-09-10 19:56:28 +0200

    working on a header

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <div className="flex justify-center bg-secondaryBg px-9">
         <p className="py-7 px-9 responsive-base h-full flex items-center">
-            &copy; 2023 Qualion. All rights reserved
+            &copy; 2023 Europe Open. All rights reserved
         </p>
     </div>
   )
--- components/navMenu/index.tsx
+++ components/navMenu/index.tsx
@@ -1,10 +1,9 @@
 "use client"
 
 import * as React from "react"
-import Link from "next/link"
+// import Link from "next/link"
 
 import { cn } from "@/lib/utils"
-// import { Icons } from "@/components/icons"
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -12,52 +11,56 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu"
+  // navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import Image from "next/image";
 
-const components: { title: string; href: string; description: string }[] = [
+const competition : { title: string; href: string; description: string }[] = [
   {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
+    title: "Participants",
+    href: "/participants",
+    description: "Browse participants",
   },
   {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
+    title: "Jury",
+    href: "/jury",
+    description: "Browse jury",
   },
   {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+    title: "Results",
+    href: "/results",
+    description: "Check results",
   },
   {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
+    title: "Rules",
+    href: "/rules",
+    description: "Competition rules",
+  },
+];
+
+const about : { title: string; href: string; description: string }[] = [
+  {
+    title: "About us",
+    href: "/about",
+    description: "Learn more about our organization",
   },
   {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+    title: "FAQ",
+    href: "/faq",
+    description: "Check out some frequently asked questions",
   },
   {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+    title: "Contact",
+    href: "/contact",
+    description: "Contact us here",
   },
-]
+];
 
 export function NavMenu() {
   return (
     <NavigationMenu>
       <NavigationMenuList>
-        <NavigationMenuItem>
+        {/* <NavigationMenuItem>
           <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
@@ -67,7 +70,6 @@ export function NavMenu() {
                     className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                     href="/"
                   >
-                    {/* <Icons.logo className="h-6 w-6" /> */}
                     <div className="mb-2 mt-4 text-lg font-medium">
                       shadcn/ui
                     </div>
@@ -89,12 +91,12 @@ export function NavMenu() {
               </ListItem>
             </ul>
           </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+        </NavigationMenuItem> */}
+        {/* <NavigationMenuItem>
+          <NavigationMenuTrigger>Competition 2</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {components.map((component) => (
+              {competition.map((component) => (
                 <ListItem
                   key={component.title}
                   title={component.title}
@@ -105,13 +107,66 @@ export function NavMenu() {
               ))}
             </ul>
           </NavigationMenuContent>
-        </NavigationMenuItem>
+        </NavigationMenuItem> */}
+        {/* COMPETITION */}
         <NavigationMenuItem>
-          <Link href="/docs" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Documentation
-            </NavigationMenuLink>
-          </Link>
+          <NavigationMenuTrigger>
+            Competition
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+          <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              {competition.map(competition => (
+                <ListItem
+                  key={competition.title}
+                  title={competition.title}
+                  href={competition.href}
+                >
+                  {competition.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        {/* ABOUT */}
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>
+            About
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+          <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <li className="row-span-3">
+                <NavigationMenuLink asChild>
+                  <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
+                  >
+                      <Image
+                          src="/europe-logo.png"
+                          alt="Europe Open logo"
+                          width={64}
+                          height={64}
+                          className="object-cover box-border overflow-hidden"
+                      />
+                    <div className="mb-2 mt-4 text-lg font-medium">
+                      Europe Open
+                    </div>
+                    <p className="text-sm leading-tight text-muted-foreground">
+                      Online Music Competition
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              {about.map(about => (
+                <ListItem
+                  key={about.title}
+                  title={about.title}
+                  href={about.href}
+                >
+                  {about.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
