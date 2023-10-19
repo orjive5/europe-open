e07b6644e3d9d6d45ba29a46ddebac3b4be8f774
German Dojcinovic <germandoicinovic@gmail.com>
2023-10-19 11:04:48 +0200

    added jury member page

--- app/(site)/jury/[member]/page.tsx
+++ app/(site)/jury/[member]/page.tsx
@@ -11,6 +11,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { Skeleton } from "@/components/ui/skeleton";
 import { getJuryMember } from "@/sanity/sanity-utils";
 import { PortableText } from "@portabletext/react";
 import { useQuery } from "@tanstack/react-query";
@@ -23,6 +24,8 @@ interface IJuryParams {
 }
 
 const JuryMember = ({params}: IJuryParams) => {
+  
+  // Get jury member data
   const { data, isLoading, isError } = useQuery(
     ['jury_member', params.member],
     () => getJuryMember(params.member)
@@ -30,9 +33,40 @@ const JuryMember = ({params}: IJuryParams) => {
 
   return (
     <section className="flex flex-col justify-center items-center">
+      {
+        isLoading && (
+          <Card className="gap-2 sm:gap-4 w-full p-2 sm:p-4 lg:w-3/4 xl:w-2/3 lg:p-8 flex flex-col sm:flex-row sm:items-start justify-center items-center text-justify">
+            <section className="flex flex-col gap-1 w-full sm:w-1/4">
+              <AspectRatio
+                className="overflow-hidden rounded-lg"
+                ratio={1 / 1}
+              >
+                <Skeleton
+                  className="h-full w-full"
+                />
+              </AspectRatio>
+              <Skeleton className="h-4 w-[150px]" />
+              <Skeleton className="h-4 w-full" />
+              <div className="gap-1 flex flex-col justify-between items-start">
+                <Skeleton className="h-4 w-[150px]" />
+                <div className="flex gap-2 flex-wrap">
+                  <Skeleton className="h-4 w-[50px]" />
+                  <Skeleton className="h-4 w-[50px]" />
+                </div>
+              </div>
+            </section>
+            <section className="w-full sm:w-3/4">
+              <Skeleton className="h-96 w-full" />
+            </section>
+          </Card>
+        )
+      }
+      {
+        isError && <h2>Something went wrong...</h2>
+      }
       {data && (
-          <Card className="w-full lg:w-3/4 xl:w-2/3 lg:p-8 flex flex-col justify-center items-center text-justify">
-            <section>
+          <Card className="gap-2 sm:gap-4 w-full p-2 sm:p-4 lg:w-3/4 xl:w-2/3 lg:p-8 flex flex-col sm:flex-row sm:items-start justify-center items-center text-justify">
+            <section className="w-full sm:w-1/4">
               <AspectRatio
                 className="overflow-hidden rounded-lg"
                 ratio={1 / 1}
@@ -46,9 +80,13 @@ const JuryMember = ({params}: IJuryParams) => {
                   className="rounded-lg object-cover"
                 />
               </AspectRatio>
-              <h2>{data.name_and_surname}</h2>
-              <h3>{data.institution}</h3>
-              <div className="gap-1 flex flex-col justify-between items-start">
+              <h2 className="text-lg md:text-2xl font-medium">
+                {data.name_and_surname}
+              </h2>
+              <h3 className="text-start">
+                {data.institution}
+              </h3>
+              <div className="gap-1 flex flex-col justify-between items-start w-full">
                 <CountrySection participant={data} />
                 <div className="flex gap-2 flex-wrap">
                   {data.discipline?.map(d => (
@@ -59,7 +97,7 @@ const JuryMember = ({params}: IJuryParams) => {
                 </div>
               </div>
             </section>
-            <section>
+            <section className="sm:w-3/4">
               <PortableText value={data.biography} />
             </section>
           </Card>
