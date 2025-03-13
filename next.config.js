cd139f3ec6b85b0f4b57eaf099309aabfe930189
germandojcinovic <germandoicinovic@gmail.com>
2025-03-14 00:03:02 +0100

    fix image upload on prod

--- next.config.js
+++ next.config.js
@@ -1,25 +1,26 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         formats: ['image/webp', 'image/avif'],
         remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'cdn.sanity.io',
-            },
-            {
-                protocol: 'https',
-                hostname: 'img.youtube.com',
-            },
-            {
-                protocol: 'http',
-                hostname: 'img.youtube.com',
-            },
+            // {
+            //     protocol: 'https',
+            //     hostname: 'cdn.sanity.io',
+            //     port: '',
+            // },
+            // {
+            //     protocol: 'https',
+            //     hostname: 'img.youtube.com',
+            //     port: '',
+            // },
+            // TODO: take a look into the this later,
+            // good practice is to use (only) remotePatterns
+            // without domains
             {
                 protocol: 'https',
                 hostname: '**.example.com',
             },
         ],
+        domains: ['img.youtube.com', 'cdn.sanity.io'],
     },
 }
 
